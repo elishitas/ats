8b954074196aedf74ae91ace230b68d68fbd4573
Maria Elisa Araya <arayamariaelisa@gmail.com>
2022-10-08 11:36:05 -0300

    expresiones basicas

--- basicos/cincouno.cpp
+++ basicos/cincouno.cpp
@@ -1,1 +1,18 @@
 //Escriba la siguiente expresion en c++ a/b + 1 
+#include<iostream>
+using namespace std;
+
+int main(){
+    float num1, num2, resultado = 0;
+
+    cout<<"Inserte su primer numero: ";
+    cin>>num1;
+    cout<<"Inserte su segundo numero: ";
+    cin>>num2;
+
+    resultado = (num1 / num2) + 1;
+
+    cout << "\nEl resultado es= " << resultado << endl;
+
+    return 0;
+}
--- basicos/seis.cpp
+++ basicos/seis.cpp
@@ -4,22 +4,29 @@
 using namespace std;
 
 int main(){
-    int num1a = 0, num2b = 0, num3c = 0, num4d = 0, suma1 = 0;
+    int num1a = 0, num2b = 0, num3c = 0, num4d = 0, suma1 = 0, suma2 = 0, division = 0;
     cout<<"Inserte un primer numero= "; cin>>num1a; 
     cout<<"Inserte un segundo numero= "; cin>>num2b;
-    //cout<<"Inserte un tercer numero= "; cin>>num3c;
-    //cout<<"Inserte un cuarto numero= "; cin>>num4d;
+    cout<<"Inserte un tercer numero= "; cin>>num3c;
+    cout<<"Inserte un cuarto numero= "; cin>>num4d;
 
-    cout<<"Su primer numero elegido es= "<<num1a<<endl; 
-    cout<<"Su segundo numero elegido es= "<<num2b<<endl; 
+    cout<<"Su primer numero elegido es= "<<num1a; 
+    cout<<"\nSu segundo numero elegido es= "<<num2b;
 
     suma1 = (num1a + num2b);
 
-    cout<<"El resultado de la suma 1 es= \n2", suma1; 
+    cout << "\nEl resultado de la suma 1 es= "<< suma1;
 
-    //int division = suma1 / suma2;
+    cout<<"\nSu tercer numero elegido es= "<<num3c; 
+    cout<<"\nSu cuarto numero elegido es= "<<num4d;
 
-    //cout<<"El resultado de la expresion pedida es= ", division; 
+    suma2 = (num3c + num4d);
+
+    cout << "\nEl resultado de la suma 2 es= "<< suma2;
+
+    division = suma1 / suma2;
+
+    cout<<"\nLa division entre la suma 1 y la suma 2 es= " << division<<endl;
 
     return 0;
 }
