ea0d19fe2333340bd9dead9773235bbafdf25854
Maria Elisa Araya <arayamariaelisa@gmail.com>
2022-10-09 23:43:38 -0300

    switch

---
+++ basicos/doce.cpp
@@ -0,0 +1,18 @@
+/*
+La sentencia switch
+
+    switch(expresion){
+        case literal1:
+            conjunto de instrucciones1;
+            break;
+        case literal2:
+            conjunto de instrucciones2;
+            break;
+        case literaln:
+            conjunto de instruccionesn;
+            break;
+        default:
+            conjunto de instrucciones por defecto;
+            break;
+        }
+*/
