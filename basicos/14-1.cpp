a45bddf6cd4c095da9d1dcb224d26a62733e327f
Maria Elisa Araya <arayamariaelisa@gmail.com>
2022-10-10 21:10:57 -0300

    num posivo / negativo

---
+++ basicos/14-1.cpp
@@ -0,0 +1,23 @@
+//Comprobar si un numero digitado por el usuario es positivo o negativo.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float numero;
+
+    cout << "Digite un numero= ";
+    cin >> numero;
+
+    if(numero == 0){
+        cout << "El numero es igual a cero";
+    }
+    else if(numero > 0){
+        cout << "El numero es positivo";
+    }
+    else
+    {
+        cout << "El numero es negativo";
+    }
+    return 0;
+}
