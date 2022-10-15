75e97a4be84441817ed6648d18e7a2533cd121fa
Elishitas <arayamariaelisa@gmail.com>
2022-10-15 17:59:30 -0300

    swith vocal

--- basicos/15.cpp
+++ basicos/15.cpp
@@ -1,1 +1,26 @@
 //Escriba un programa que lea de la entrada estandar un caracter e indique en la salida estandar si el caracter es una vocal minuscula o no.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char vocal;
+
+    cout << "Escriba una vocal= ";
+    cin >> vocal;
+
+    switch (vocal)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    cout << "Usted escribio la siguiente vocal en minuscula: " << vocal; break;
+    default: cout << "La vocal que escribio no es vocal o se encuentra en mayuscula"; break;
+    }
+
+    return 0;
+
+}
