f14c5ff284fd3c9ecef342c8163aa98e555bbf1d
Elishitas <arayamariaelisa@gmail.com>
2022-10-18 22:49:27 -0300

    numero romanos switch

---
+++ basicos/17.cpp
@@ -0,0 +1,22 @@
+/*Cambiar un numero entero con el mismo valor pero en numeros romanos =
+
+M = 1000
+D = 500
+C = 100
+L = 50
+X = 10
+V = 5
+I = 1
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero;
+
+    cout << "Digite un numero"; cin >> numero;
+
+    return 0;
+}
